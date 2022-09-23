8e823343e8a1a81842cb9c6fbc47f7cfbb97a355
igwe felix <felixigwe55@gmail.com>
2022-09-23 14:57:26 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,6 @@ nclude "main.h"
  * @src: pointer to source input
  *
  * Return: pointer to resulting string @dest
- *
  */
 
 char *_strcat(char *dest, char *src)
@@ -17,12 +16,10 @@ char *_strcat(char *dest, char *src)
 
 	c = 0;
 	/*find the size of dest array*/
-
 	while (dest[c])
 		c++:
 
 	/* iterate through each src array value without the null byte*/
-
 	for (c2 = 0; src[c2] ; c2++)
 		/*append src[c2] to dest[c] while overwritting the null byte in dest*/
 		dest[c++] = src[c2];

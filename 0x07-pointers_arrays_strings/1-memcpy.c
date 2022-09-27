3d1597d50c6beaf2997e92cc2b063ebc6ed12d58
igwe felix <felixigwe55@gmail.com>
2022-09-27 07:03:13 -0700

    functions

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,9 +9,16 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i:
+	int size = r;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) =  *(src + i);
+	if (size > 0)
+	{
+		int m;
+
+		for (m = 0; m < size; m++)
+		{
+			dest[m] = src[m];
+		}
+	}
 	return (dest);
 }

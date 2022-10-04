8b2baee0fcc136843b30b6da15ae4353a1904f8f
igwe felix <felixigwe55@gmail.com>
2022-10-04 09:08:19 -0700

    argc andargv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the of args
+ * @argc: Argument count
+ * @argv: Array of argument strings
+ *
+ * Return: 0 for succesful exit
+ */
+int main(int argc, char *argv[])
+{
+	int j;
+
+	for (j = 0; j < argc; j++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
